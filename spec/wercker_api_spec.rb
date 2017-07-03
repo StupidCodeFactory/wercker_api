2f696ba310755c544f8bc5065fbd2693cdbf61c7
yann marquet <ymarquet@gmail.com>
2017-07-03 22:23:47 +0100

    Fix VERSION

--- spec/wercker_api_spec.rb
+++ spec/wercker_api_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 RSpec.describe WerckerAPI do
   it "has a version number" do
-    expect(WerckerApi::VERSION).not_to be nil
+    expect(WerckerAPI::VERSION).not_to be nil
   end
 
 end
