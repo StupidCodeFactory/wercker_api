0815954b4a5f67bcd532cb7b2bf6e6722c5e78de
yann marquet <ymarquet@gmail.com>
2017-07-05 00:16:54 +0100

    Timeout error with meanigful message upon failure

--- lib/wercker_api/pipeline_runner.rb
+++ lib/wercker_api/pipeline_runner.rb
@@ -1,8 +1,32 @@
 module WerckerAPI
   class PipelineRunner
-    def initialize(client, max_attempt = 20, delay = 10)
+
+    class Timeout < RuntimeError
+      def initialize(pipeline_id, pipeline_runner)
+        msg = <<-EOM
+Pipeline #{pipeline_id} did not finish in a timely fashion.
+
+#{pipeline_runner.max_attempts} attempts were made with a #{pipeline_runner.delay} seconds delay between each attempts.
+
+You  either can try to:
+    - Increase the max attempts count.
+      Beware of the API rate limit, specially
+      if you have many applications and pipelines
+      under the same account.
+    - the delay between each attempts
+    - or both
+
+Good Luck!
+EOM
+        super(msg)
+      end
+    end
+
+    attr_accessor :delay, :max_attempts
+
+    def initialize(client, max_attempts: 20, delay: 10)
       self.client          = client
-      self.max_attempt     = max_attempt
+      self.max_attempts     = max_attempts
       self.delay           = delay
       self.current_attempt = 0
     end
@@ -10,19 +34,21 @@ module WerckerAPI
     def run(pipeline_id, trigger_run_params = {})
       run = client.trigger_run pipeline_id, trigger_run_params
 
-      while %w[running notstarted].include?(run.status) && !max_attempt_reached?
-        sleep delay
+      while %w[running notstarted].include?(run.status)
+        raise Timeout.new(pipeline_id, self) if max_attempt_reached?
+        sleep @delay
         run = client.run(run.id)
         @current_attempt += 1
       end
+
       run
     end
 
     private
 
-    attr_accessor :client, :max_attempt, :delay, :current_attempt
+    attr_accessor :client, :current_attempt
     def max_attempt_reached?
-      current_attempt >= max_attempt
+      current_attempt >= max_attempts
     end
   end
 end
--- spec/wercker_api/pipeline_runner_spec.rb
+++ spec/wercker_api/pipeline_runner_spec.rb
@@ -1,12 +1,44 @@
 require 'spec_helper'
 
 RSpec.describe WerckerAPI::PipelineRunner do
-  let(:client) { WerckerAPI::Client.new }
+  let(:client)      { WerckerAPI::Client.new }
   let(:pipeline_id) { '595a6b1b24ac030100da5307' }
+  let(:args)        { [client] }
 
-  subject { described_class.new(client) }
+  subject { described_class.new(*args) }
 
-  it 'runs the pipeline and waited until finished', vcr: { cassette_name: :run_pipeline } do
-    expect(subject.run(pipeline_id, branch: 'pipeline-runner').status).to eq('finished')
+  describe '#run' do
+    context 'when the run finishes' do
+      it 'runs the pipeline and waited until finished', vcr: { cassette_name: :run_pipeline } do
+        expect(subject.run(pipeline_id, branch: 'pipeline-runner').status).to eq('finished')
+      end
+    end
+
+    context 'when the run times out', vcr: { cassette_name: :run_pipeline_with_timeout } do
+      # need to finish within 2 attemtps with a 2 second delay between each
+      #  so build will timeout after around 4 seconds
+      let(:args) { [client, max_attempts: 2, delay: 2] }
+
+      it 'raise an excpetion' do
+        expect {
+          subject.run(pipeline_id, branch: 'pipeline-runner')
+        }.to raise_error(RuntimeError, <<-EOM)
+Pipeline #{pipeline_id} did not finish in a timely fashion.
+
+2 attempts were made with a 2 seconds delay between each attempts.
+
+You  either can try to:
+    - Increase the max attempts count.
+      Beware of the API rate limit, specially
+      if you have many applications and pipelines
+      under the same account.
+    - the delay between each attempts
+    - or both
+
+Good Luck!
+EOM
+      end
+    end
   end
+
 end
