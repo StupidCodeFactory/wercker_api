0788a84037fe488dc9e851359f768df8d6eaa379
Yann Marquet <yann.marquet@digital.justice.gov.uk>
2017-07-04 18:18:36 +0100

    Fix APICollection and build a pipeline runner

--- lib/wercker_api/api_collection.rb
+++ lib/wercker_api/api_collection.rb
@@ -12,11 +12,17 @@ module WerckerAPI
 
 
     def initialize(collection = [])
-      self.collection = collection
+      self.collection = collection.map { |item| item_klass.new(item) }
     end
 
-    def each
-      collection.each
+    def each(&block)
+      byebug
+      collection.each(&block)
+    end
+    private
+
+    def item_klass
+      @klass ||= Kernel.const_get(self.class.name.sub(/Collection$/, ''))
     end
   end
 end
---
+++ lib/wercker_api/pipeline_runner.rb
@@ -0,0 +1,29 @@
+module WerckerAPI
+
+  class PipelineRunner
+
+    def initialize(client, max_attempt = 20, delay = 10)
+      self.client          = client
+      self.max_attempt     = max_attempt
+      self.delay           = delay
+      self.current_attempt = 0
+    end
+
+    def run(pipeline_id)
+      run = client.trigger_run pipeline_id
+
+      while ['running', 'notstarted'].include?(run.status) && !max_attempt_reached?
+        sleep delay
+        run = client.run(run.id)
+        self.current_attempt += 1
+      end
+      run
+    end
+
+    private
+    attr_accessor :client, :max_attempt, :delay, :current_attempt
+    def max_attempt_reached?
+      current_attempt >= max_attempt
+    end
+  end
+end
