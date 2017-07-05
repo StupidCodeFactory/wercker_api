67c4eb6933a996701fc091c0873d283666c41f72
yann marquet <ymarquet@gmail.com>
2017-07-05 19:50:41 +0100

    Fix test

--- lib/wercker_api/run/step.rb
+++ lib/wercker_api/run/step.rb
@@ -13,7 +13,28 @@ module WerckerAPI
       attribute :finishedAt,   Time
       attribute :startedAt,    Time
       attribute :status,       String
+      attribute :result,       String
       attribute :step,         String
+
+      def artifacts_url
+        artifactsUrl
+      end
+
+      def log_url
+        logUrl
+      end
+
+      def created_at
+        createdAt
+      end
+
+      def started_at
+        startedAt
+      end
+
+      def finished_at
+        finishedAt
+      end
     end
   end
 end
